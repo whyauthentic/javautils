6ac7756fd473cddac95cc6d9c6660feadb1294b4
whyauthentic <business.whyauthentic@gmail.com>
2023-05-28 00:59:09 +0200

    Changed ConsoleMessage to AnsiColors instead of ConsoleColors

--- src/main/java/me/whyauthentic/javautils/ConsoleMessages.java
+++ src/main/java/me/whyauthentic/javautils/ConsoleMessages.java
@@ -4,23 +4,23 @@ package me.whyauthentic.javautils;
 public class ConsoleMessages {
 
     public static void SYSTEM(String message) {
-        System.out.println(ConsoleColors.CYAN_BOLD + "[SYSTEM] " + message + ConsoleColors.RESET);
+        System.out.println(AnsiColors.CYAN_BOLD + "[SYSTEM] " + message + AnsiColors.RESET);
     }
 
     public static void INFO(String message) {
-        System.out.println(ConsoleColors.GREEN_BOLD + "[INFO] " + message + ConsoleColors.RESET);
+        System.out.println(AnsiColors.GREEN_BOLD + "[INFO] " + message + AnsiColors.RESET);
     }
 
     public static void ERROR (String message) {
-        System.out.println(ConsoleColors.RED_BOLD + "[ERROR] " + message + ConsoleColors.RESET);
+        System.out.println(AnsiColors.RED_BOLD + "[ERROR] " + message + AnsiColors.RESET);
     }
 
    public static void WARNING (String message) {
-        System.out.println(ConsoleColors.YELLOW_BOLD + "[WARNING] " + message + ConsoleColors.RESET);
+        System.out.println(AnsiColors.YELLOW_BOLD + "[WARNING] " + message + AnsiColors.RESET);
    }
 
    public static void DEBUG (String message) {
-        System.out.println(ConsoleColors.PURPLE_BOLD + "[DEBUG] " + message + ConsoleColors.RESET);
+        System.out.println(AnsiColors.PURPLE_BOLD + "[DEBUG] " + message + AnsiColors.RESET);
    }
 
 }
